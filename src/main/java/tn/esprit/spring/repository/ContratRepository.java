cd0758091c9b7f872970c3ad630e9e183ecd3c70
Slim DRIDI <slim.dridi@esprit.tn>
2021-05-11 17:56:58 +0200

    initialisation de notre projet

---
+++ src/main/java/tn/esprit/spring/repository/ContratRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tn.esprit.spring.entities.Contrat;
+
+public interface ContratRepository extends JpaRepository<Contrat, Integer>{
+
+}
